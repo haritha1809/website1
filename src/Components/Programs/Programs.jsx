4ab180b7d7f00bcac890ab889debc5cb0eac667f
aiswariya96 <aiswariyasivakumar9405@gmail.com>
2025-07-04 15:36:01 +0530

    added

--- src/Components/Programs/Programs.jsx
+++ src/Components/Programs/Programs.jsx
@@ -1,32 +1,34 @@
-import React from 'react'
-import "./Programs.css"
-import {programsData} from "../../data/programsData"
-import RightArrow from "../../assets/rightArrow.png"
+import React from "react";
+import "./Programs.css";
+import { programsData } from "../../data/programsData";
+import RightArrow from "../../assets/rightArrow.png";
 
-const Programs= () => {
+const Programs = () => {
   return (
-    <div className="Programs" id="programs">
-        <div className="programs-header">
-            <span className='stroke-text'>Explore Our</span>
-            <span>Programs</span>
-            <span className='stroke-text'>to shape you</span>
-        </div>
-        <div className="programs-categories">
-            {programsData.map((program)=>(
-                <div className="category">
-                    {program.image}
-                    <span>{program.heading}</span>
-                    <span>{program.details}</span>
-                    <div className="join-now">
-                        <span>Join Now</span>
-                        <img src={RightArrow} alt="Right Arrow"/>
-                    </div>
-                </div>
-            ))}
-        </div>
-    </div>
+    <>
+      <div className="programs-header">
+        <h2 className="programs-title">
+          <span className="stroke-text">Explore Our </span>
+          <span className="highlight-text">PROGRAMS </span>
+          <span className="stroke-text">to shape you</span>
+        </h2>
+      </div>
 
-  )
-}
+      <div className="programs-categories">
+        {programsData.map((program, index) => (
+          <div className="category" key={index}>
+            {program.image}
+            <span>{program.heading}</span>
+            <span>{program.details}</span>
+            <div className="join-now">
+              <span>Join Now</span>
+              <img src={RightArrow} alt="Right Arrow" />
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default Programs
+export default Programs;
